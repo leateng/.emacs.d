eda146dfa040324c97931379adf60ef80549d742
teng.li <teng.li@yhlsoft.com>
2020-09-03 19:06:29 +0800

    init commit

---
+++ elpa/ruby-extra-highlight-20171106.1933/ruby-extra-highlight-autoloads.el
@@ -0,0 +1,22 @@
+;;; ruby-extra-highlight-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "ruby-extra-highlight" "ruby-extra-highlight.el"
+;;;;;;  (24400 30438 198210 900000))
+;;; Generated autoloads from ruby-extra-highlight.el
+
+(autoload 'ruby-extra-highlight-mode "ruby-extra-highlight" "\
+Minor mode that highlights method and block parameters.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ruby-extra-highlight-autoloads.el ends here
---
+++ init.el
@@ -0,0 +1,46 @@
+;; Set up package.el to work with MELPA
+(require 'package)
+(setq package-archives '(("gnu" . "http://elpa.emacs-china.org/gnu/")
+                         ("melpa" . "http://elpa.emacs-china.org/melpa/")
+                         ("melpa-stable" . "http://elpa.emacs-china.org/melpa-stable/")
+                         ("marmalade" . "http://elpa.emacs-china.org/mlade/")
+                         ("org" . "http://elpa.emacs-china.org/org/")
+                         ("sunrise-commander" . "http://elpa.emacs-china.org/sunrise-commander/")
+                         ("user42" . "http://elpa.emacs-china.org/user42/")))
+(package-initialize)
+
+;; Download Evil
+;;(unless (package-installed-p 'evil)
+;;  (package-install 'evil))
+
+;; Enable Evil
+;;(require 'evil)
+;;(evil-mode 1)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("e1d09f1b2afc2fed6feb1d672be5ec6ae61f84e058cb757689edb669be926896" "4cf9ed30ea575fb0ca3cff6ef34b1b87192965245776afa9e9e20c17d115f3fb" "aded61687237d1dff6325edb492bde536f40b048eab7246c61d5c6643c696b7f" "939ea070fb0141cd035608b2baabc4bd50d8ecc86af8528df9d41f4d83664c6a" default)))
+ '(package-selected-packages
+   (quote
+    (rinari yasnippet-snippets yasnippet inf-ruby ruby-extra-highlight ruby-end ruby-electric gruvbox-theme theme-changer material-theme evil-visual-mark-mode ag ruby-test-mode undohist evil))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; hide menu bar
+(menu-bar-mode -1)
+;; hide tool bar
+(tool-bar-mode -1)
+;; highlight cursor line
+(hl-line-mode t)
+;; show line number
+(global-linum-mode t)
+;; use gruvbox-theme
+(load-theme 'gruvbox-dark-soft t)
